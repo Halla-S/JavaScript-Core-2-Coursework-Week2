c326ef46da4f040ee0e7e23c2a676d382fa469f6
Halla-S <halla.sulaiman.33@gmail.com>
2021-09-30 12:59:34 +0100

    Exercise B

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,4 +1,14 @@
 function shoppingList(arrayOfPeople) {
+  let content = document.querySelector("#content");
+  let ul = document.createElement("ul");
+  for (var i = 0; i < arrayOfPeople.length; i++) {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(arrayOfPeople[i]));
+    //li.textContent = arrayOfPeople[i]; It can be done this way as well
+    ul.appendChild(li);
+  }
+
+  content.appendChild(ul);
   // Write your code here...
 }
 
