62217c454f3ed3bbde1675fbb52e2e30f9d2f42b
Halla-S <halla.sulaiman.33@gmail.com>
2021-09-30 12:59:10 +0100

    Exercise A

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,14 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach((element) => {
+    let newh1 = document.createElement("h1");
+    let newh2 = document.createElement("h2");
+    newh1.textContent = element.name;
+    newh2.textContent = element.job;
+    content.appendChild(newh1);
+    content.appendChild(newh2);
+  });
+
 }
 
 let people = [
