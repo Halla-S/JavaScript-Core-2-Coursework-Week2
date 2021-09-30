bcaa034f78ea7e41e4bcb35c26a3282b47013788
Halla-S <halla.sulaiman.33@gmail.com>
2021-09-30 13:00:02 +0100

    Exercise C

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,4 +1,33 @@
 function readingList(books) {
+  let content = document.querySelector("#content");
+  
+  let ul = document.createElement("ul");
+  for (var i=0; i<books.length; i++){
+    let li = document.createElement("li");
+    let pelement = document.createElement("p");
+    let img = document.createElement("img");
+    //img.src =coverPage;(CAN NOT FIND THE COVERPAGR HERE !!!!)
+    pelement.textContent = books[i].title + " by " + books[i].author;
+    let img = document.createElement("img");
+    
+    if (books[i].alreadyRead === true) {
+      li.style.background = "Green";
+    } else {
+      li.style.background = "Red";
+    }
+    li.appendChild(img);
+    li.appendChild(pelement);
+    li.style.width = "100px";
+    li.style.height = "100px";
+    li.style.fontSize = "7px";
+    img.width = "50";
+    ul.style.listStyleType = "none";
+    li.style.float = "left";
+    li.style.display = "inline";
+    li.style.marginRight = "20px";
+    ul.appendChild(li);
+  }
+  content.appendChild(ul)
   // Write your code here...
 }
 
