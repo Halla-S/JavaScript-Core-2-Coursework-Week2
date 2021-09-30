87bc383487834a93b221f856619fab38d4a6e58b
Halla-S <halla.sulaiman.33@gmail.com>
2021-09-30 13:00:43 +0100

    Exercise E

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,4 +1,29 @@
 function listOfColours(colours) {
+  let content = document.getElementById("content");
+  let selectElement = document.createElement("select");
+
+  let pElement = document.createElement("p");
+  pElement.innerText = "Can you see my colour ?";
+  content.appendChild(pElement);
+ 
+  
+  for (var i =0; i<colours.length; i++){
+    let optionElement = document.createElement("option");
+    optionElement.setAttribute("value" ,colours[i])
+  
+    let text = document.createTextNode(
+      (optionElement.value)
+    );
+    selectElement.addEventListener("click", changecolour );
+    function changecolour(){
+    pElement.style.color = selectElement.value;
+    } 
+
+    //pElement.appendChild(text);
+    optionElement.appendChild(text);
+    selectElement.appendChild(optionElement);
+  }
+  content.appendChild(selectElement);
   // Write your code here...
 }
 
