b108f85302ee4d6c88c5f0cc1b5f6ec7b8e37755
Halla-S <halla.sulaiman.33@gmail.com>
2021-09-30 13:00:20 +0100

    Exercise D

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,43 @@
 function todoList(todos) {
-  // Write your code here...
+  let content = document.getElementById("content");
+  let ul = document.createElement("ul");
+  content.appendChild(ul);
+  todos.forEach(element => {
+    let li = document.createElement("li");
+    let text = document.createTextNode(element.todo);
+    //console.log(element.todo);
+      li.addEventListener("click", check);
+      function removelinethrough() {
+        li.style.textDecoration = "none";
+      }
+
+      function linethrough() {
+        li.style.textDecoration = "line-through";
+      }
+
+      function check(){
+        
+    if(li.style.textDecoration= "line-through"){
+      li.addEventListener("click", removelinethrough);
+    }
+
+    
+    else{
+     li.addEventListener("click", linethrough);
+    }
+    
+  }
+
+    
+
+    li.appendChild(text);
+    ul.appendChild(li);
+  });
+  
+  content.appendChild(ul);
+  
+
+
 }
 
 const todos = [
